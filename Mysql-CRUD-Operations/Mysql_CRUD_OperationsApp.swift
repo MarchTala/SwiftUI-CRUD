a85ef032348ef0eb1154f9ede0dc519bbc941ebd
MT String <marchjigtala@yahoo.com>
2022-03-19 08:43:30 +0800

    Initial Commit

---
+++ Mysql-CRUD-Operations/Mysql_CRUD_OperationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mysql_CRUD_OperationsApp.swift
+//  Mysql-CRUD-Operations
+//
+//  Created by March Tala on 3/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Mysql_CRUD_OperationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mysql-CRUD-OperationsTests/Mysql_CRUD_OperationsTests.swift
@@ -0,0 +1,33 @@
+//
+//  Mysql_CRUD_OperationsTests.swift
+//  Mysql-CRUD-OperationsTests
+//
+//  Created by March Tala on 3/19/22.
+//
+
+import XCTest
+@testable import Mysql_CRUD_Operations
+
+class Mysql_CRUD_OperationsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
